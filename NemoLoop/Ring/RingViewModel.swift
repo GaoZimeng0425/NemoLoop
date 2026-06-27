0d335e34740d90dc6f54fe7dc574977761712cf2
GaoZimeng <gaozimeng0425@gmail.com>
2026-06-28 00:59:52 +0800

    feat: add RingViewModel with mouse-poll highlight loop

---
+++ NemoLoop/Ring/RingViewModel.swift
@@ -0,0 +1,43 @@
+import AppKit
+import Foundation
+import Observation
+
+@MainActor
+@Observable
+final class RingViewModel {
+    var isShown = false
+    var highlightedIndex: Int?
+    let deadZoneRadius: CGFloat = 36
+
+    @ObservationIgnored private var centerGlobal: CGPoint = .zero
+    @ObservationIgnored private var timer: Timer?
+
+    var selectedIndex: Int? { highlightedIndex }
+
+    func begin(centerGlobal: CGPoint) {
+        self.centerGlobal = centerGlobal
+        self.highlightedIndex = nil
+        self.isShown = true
+        timer?.invalidate()
+        let t = Timer(timeInterval: 1.0 / 120.0, repeats: true) { [weak self] _ in
+            Task { @MainActor in self?.sample() }
+        }
+        RunLoop.main.add(t, forMode: .common)
+        timer = t
+    }
+
+    func end() {
+        timer?.invalidate()
+        timer = nil
+        isShown = false
+        highlightedIndex = nil
+    }
+
+    private func sample() {
+        highlightedIndex = RingGeometry.wedgeIndex(
+            from: centerGlobal,
+            to: NSEvent.mouseLocation,
+            deadZoneRadius: deadZoneRadius
+        )
+    }
+}
